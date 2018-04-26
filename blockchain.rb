7d8eae9c9e64b35f49f703c929f43fb003c919e1
LAPTOP-GH9U4H3D\ejl1 <forbrignterworld@gmail.com>
2018-04-26 21:48:18 +0900

    논스 SHA256

--- blockchain.rb
+++ blockchain.rb
@@ -1,10 +1,32 @@
-
+require 'digest' # 특정한 문자열을 소화해서 다른 문자열로 나타냄 = 암호화함.
 
 class Blockchain 
 
- attr_reader :chain, :transactions
+	def initialize #initialize는 블록체인이라는 클래스가 생성이 될 때 무조건 실행 됨, 저장되는 값이 필요할때 initialize가 필요함. @들어가는 지워지지 않는 값들 생성.
+	@number_of_blocks = 1 #@이가 붙으면 클래스 전체에서 쓰임.
+	end
+
+ def mining
+ 	@number_of_blocks += 1 #@이가 붙어서 영원히 지워지지 않고 계속감.
+  # 브라우저에 찍을거라 puts 안씀 마지막은 결과로 바치는 것
+   history = []
+   if @number_of_blocks < 100 # 반감기 설정 헤헤
+   begin
+   	nonce = rand(1000000)
+   	hashed = Digest::SHA256.hexdigest(nonce.to_s) #콜론 2개 Digest::SHA256 샤256 끌어오는것 루비는 hash key value로 쓰임 .hexdigest 는 암호화를 끌어오는 메소드 
+
+
+
+   	history << nonce # history라는 리스트에 nonce 값을 밀어넣겠다. << 이 .push(nonce)랑 같음.
+   end while hashed[0..3] != '0000' # =이 =>이런 뜻이라고 생각하셈. ==은 같냐고 물어보는 것. != 아니니까 한바퀴 도는거임 ==이면 끝남.
+   # [0..3] 0부터 3번째 자리까지를 의미
+   history
+ end
+
+
+ def my_blocks
+ 	@number_of_blocks
+ end
+
 
 end
-
-joon = Blockchain.new
-puts joon
--- server.rb
+++ server.rb
@@ -1,23 +1,13 @@
-# encoding: UTF-8
-
 require 'sinatra'
+require './blockchain' #./는 같은 디렉토리에 있다는 뜻. 같은 디렉토리에 있어야 작동이 된다. 복사해서 넣는거랑 똑같음.
+
+b = Blockchain.new
 
 get '/' do 
-	"block chain!!"
+	"지금 전체 블럭수는 :" + b.my_blocks.to_s
 end
 
 get '/mine' do
-	"you mine a block"
-end
+	"논스값을 찾았습니다!(레알!)" + b.mining.to_s # to_s숫자와 문자로 계산할때 숫자나 문자로 통일해야지 계산이 됨, to striong의 약자. 다 문자로 바꾸는것.
 
-get '/transaction' do
-	"Insert your Txid"
-end
-
-get '/hello' do
-	"!"
-end
-
-get '/ejl1' do
-	"이의준"
 end
